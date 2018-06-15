571e6aa0327ec8067fda541c27a0c1078d8116d7
amyhenning <henning.amy@gmail.com>
2018-06-15 05:09:43 +0000

    Installed pry gem to try to debug error

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -28,6 +28,7 @@ class Instructor::CoursesController < ApplicationController
 
 	helper_method :current_course
 	def current_course
+	#	binding.pry
 		@current_course ||= Course.find(params[:id])
 	end
 
